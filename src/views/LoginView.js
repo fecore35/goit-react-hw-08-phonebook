b591f9d7e007996e451e9d7d5124bb3e4a257266
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-11-28 07:28:24 +0200

    Add LoginForm

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,5 +1,13 @@
+import LoginForm from "components/LoginForm";
+
 function LoginView() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <h1>Login</h1>
+
+      <LoginForm />
+    </>
+  );
 }
 
 export default LoginView;
