f5e9ae8faff22ab62c1d0f898dcd1ade142609e2
Bryzgun <bryzgun@Evgenii-Bryzgun-MacBook-Pro.local>
2021-12-01 17:56:59 +0200

    Add @mui/material for RegisterForm

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,56 +1,91 @@
 import { useDispatch } from "react-redux";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useFormik } from "formik";
 import { authOperations } from "redux/auth";
+
+/* yup: value validation */
+import * as yup from "yup";
+
+/* Material UI */
+import { Stack, Box, Button, TextField } from "@mui/material";
 
 function RegisterForm() {
   const dispatch = useDispatch();
 
-  const setValidate = (values) => {
-    const errors = {};
-
-    if (!values.name) {
-      errors.name = "Required";
-    }
-
-    if (!values.password) {
-      errors.password = "Required";
-    }
-
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = "Invalid email address";
-    }
-
-    return errors;
+  const onRegister = (values, { setSubmitting }) => {
+    dispatch(authOperations.register(values));
+    setSubmitting(false);
   };
 
+  const setValidate = yup.object({
+    name: yup.string("Enter name").required("Name is required"),
+    email: yup
+      .string("Enter your email")
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: yup
+      .string("Enter your password")
+      .min(8, "Password should be of minimum 8 characters length")
+      .required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      number: "",
+    },
+    validationSchema: setValidate,
+    onSubmit: onRegister,
+  });
+
   return (
-    <Formik
-      initialValues={{ name: "", email: "", password: "" }}
-      validate={setValidate}
-      onSubmit={(values, { setSubmitting }) => {
-        dispatch(authOperations.register(values));
-        setSubmitting(false);
-      }}
+    <Box
+      component="form"
+      onSubmit={formik.handleSubmit}
+      noValidate
+      autoComplete="off"
     >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="text" name="name" />
-          <ErrorMessage name="name" component="div" />
-
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="stretch"
+        spacing={2}
+      >
+        <TextField
+          fullWidth
+          id="name"
+          name="name"
+          label="Name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Login
+        </Button>
+      </Stack>
+    </Box>
   );
 }
 
